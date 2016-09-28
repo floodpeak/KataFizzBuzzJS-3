011700c729a2513de3c04d9cd95fe2829c313b10
floodpeak <hongfengzhou@yahoo.com>
2016-09-28 11:32:03 +0800

    add test for 2

--- test/getNumberMasked.test.js
+++ test/getNumberMasked.test.js
@@ -5,4 +5,7 @@ describe('number masked',function(){
   it('should be 1 for 1', function(){
     expect(getNumberMasked(1)).to.be.equal(1)
   })
+  it('should be 2 for 2', function(){
+    expect(getNumberMasked(2)).to.be.equal(2)
+  })
 })
