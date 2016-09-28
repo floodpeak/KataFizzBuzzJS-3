30c27891b1cd1cc285b64e5728ae36b68e401e24
floodpeak <hongfengzhou@yahoo.com>
2016-09-28 12:13:53 +0800

    pull the mask strings out

--- getNumberMasked.js
+++ getNumberMasked.js
@@ -1,14 +1,16 @@
   var base1 = 3
   var base2 = 5
+  var mask1 = 'Fizz'
+  var mask2 = 'Buzz'
 module.exports = function(number){
   if(number % (base1*base2) == 0 ){
-    return 'FizzBuzz'
+    return mask1 + mask2
   }
   if(number % base1 == 0){
-    return 'Fizz'
+    return mask1
   }
   if(number % base2 == 0){
-    return 'Buzz'
+    return mask2
   }
   return number
 }
