92466a58dec7de4e971d101488f7f3c2019039be
floodpeak <hongfengzhou@yahoo.com>
2016-09-28 12:15:23 +0800

    modify condition of multiples of base1 and base2

--- getNumberMasked.js
+++ getNumberMasked.js
@@ -2,8 +2,9 @@
   var base2 = 5
   var mask1 = 'Fizz'
   var mask2 = 'Buzz'
+
 module.exports = function(number){
-  if(number % (base1*base2) == 0 ){
+  if(number % base1 == 0 && number % base2 == 0 ){
     return mask1 + mask2
   }
   if(number % base1 == 0){
